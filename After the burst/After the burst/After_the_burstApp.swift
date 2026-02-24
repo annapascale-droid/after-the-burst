62d8eaf964d726a03a1489b78a88c1dfa1618ce3
anna pascale <na>
2026-02-24 11:33:34 +0100

    first  commit

---
+++ After the burst/After the burst/After_the_burstApp.swift
@@ -0,0 +1,17 @@
+//
+//  After_the_burstApp.swift
+//  After the burst
+//
+//  Created by Foundation 16 on 24/02/26.
+//
+
+import SwiftUI
+
+@main
+struct After_the_burstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ After the burst/After the burst/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  After the burst
+//
+//  Created by Foundation 16 on 24/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
